2a72b01f4c451d708a0ea74b0bf80fa0b916e8be
All-Star2 <allst4r112@outlook.com>
2024-10-05 18:43:30 -0400

    Create PhantasmTSB.lua

---
+++ PhantasmTSB.lua
@@ -0,0 +1,4 @@
+getgenv().ToggleKeybind = Enum.KeyCode.RightControl
+getgenv().FreeEmotesTab = true
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ATrainz/main/main/Phantasm-Loader.lua"))()
